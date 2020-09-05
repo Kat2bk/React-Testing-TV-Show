a4da846b22ea26d782f7162946d98854857e27b3
unknown <Katherine2bk@gmail.com>
2020-09-05 12:39:39 -0500

    updated errors... tests are passing could be false posiives. api wasn't working because of repeated data calls

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 
 export const fetchShow = () => {
@@ -5,8 +6,8 @@ export const fetchShow = () => {
     .get(
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
-    // .then(res => {
-    //   console.log("response", res);
-    //   return res;
-    // })
+    .then(res => {
+      console.log("response from axios", res)
+      return res
+    })
 };
